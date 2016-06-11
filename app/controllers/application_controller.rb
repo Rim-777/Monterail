e8ca7443c49c8d00a23788d13f5c37d9dc391717
Rim-777 <javatimomoss@gmail.com>
2016-06-11 13:33:51 +0300

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+  # We don't want to overcomplicate, so let this be just disabled
+  # protect_from_forgery with: :exception
+end
---
+++ app/models/operation.rb
@@ -0,0 +1,7 @@
+class Operation < ActiveRecord::Base
+  belongs_to :company
+
+  validates_presence_of :invoice_num, :invoice_date, :amount, :operation_date, :kind, :status
+  validates_numericality_of :amount, greater_than: 0
+  validates_uniqueness_of :invoice_num
+end
