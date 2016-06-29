b589c0d9a9d6f546f4452f34af3139c3af45dc70
Rim-777 <javatimomoss@gmail.com>
2016-06-29 14:31:07 +0300

    Add JS WebSocket

--- lib/web_socket_server.rb
+++ lib/web_socket_server.rb
@@ -1,37 +1,17 @@
 require 'em-websocket'
-require 'em-hiredis'
 
-EM.run do
-
-    @channel = EM::Channel.new
-
-    @redis = EM::Hiredis.connect
-    puts 'subscribing to redis'
-    @redis.subscribe
-    @redis.on(:message){|channel, message|
-      puts "redis -> #{channel}: #{message}"
-      @channel.push message
-    }
-
-    # Creates a websocket listener
-
+EM.run {
   EM::WebSocket.run(:host => "127.0.0.1", :port => 9000) do |ws|
     ws.onopen { |handshake|
       puts "WebSocket connection open"
 
-      # Access properties on the EM::WebSocket::Handshake object, e.g.
-      # path, query_string, origin, headers
-
-      # Publish message to the client
-      ws.send "Hello Client, you connected to #{handshake.path}"
+      ws.send "the server has confirmed the connection by url: #{handshake.path}"
     }
-
     ws.onclose { puts "Connection closed" }
 
     ws.onmessage { |msg|
       puts "Recieved message: #{msg}"
-      @channel.push msg
       ws.send "Pong: #{msg}"
     }
   end
-end
+}
